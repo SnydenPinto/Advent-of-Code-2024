ae8085ab20793bb927507297e1000bb734ba7336
Snyden Pinto <Snyden7@gmail.com>
2024-12-02 17:31:50 +0000

    day one part two solution

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def read_and_sort(filename):
     with open(filename, 'r') as file:
         data = [tuple(map(int, line.split())) for line in file]
@@ -10,8 +13,26 @@ def read_and_sort(filename):
 def calculate_total_distance(left_column, right_column):
     return sum(abs(left - right) for left, right in zip(left_column, right_column))
 
+def calculate_similarity_score(left, right):
+    frequency_map = defaultdict(int)
+
+    for num in right:
+        frequency_map[num] += 1
+
+    score = 0
+    for num in left:
+        score += num * frequency_map[num]
+
+    return  score
+
 
 if __name__ == "__main__":
     left, right = read_and_sort('input')
     distance = calculate_total_distance(left, right)
-    print(distance)
+    similarity_score = calculate_similarity_score(left,right)
+
+    print("Day 1 - Part One :",distance)
+    print("Day 1 - Part Two :", similarity_score)
+
+
+
